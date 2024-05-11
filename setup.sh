d4d1be33de37c4f1270c3b7c9bc449dc2fb9793f
Victor Barbarich <victor.barbarich@dif.tech>
2024-05-12 00:18:51 +0300

    adj for macbook

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ conda activate llama_hw
 # As written, this command assumes you have CUDA on your machine, but
 # refer to https://pytorch.org/get-started/previous-versions/ for the correct
 # command for your system.
-conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=11.8 -c pytorch -c nvidia
+conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 -c pytorch
 pip install tqdm==4.66.1
 pip install requests==2.31.0
 pip install importlib-metadata==3.7.0
